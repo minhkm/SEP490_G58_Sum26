c223e4b15155bcb2c4ae438c82e4e7188fec20b4
minhkm <minhtnhe180070@fpt.edu.vn>
2026-06-02 17:18:08 +0700

    Initial project setup

---
+++ backend/readme.md
@@ -0,0 +1,10 @@
+# CargoOps
+
+Cargo Management System
+
+## Tech Stack
+
+- React + Vite
+- React Bootstrap
+- NodeJS Express
+- MongoDB Atlas
---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Container, Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container className="mt-5">
+      <h1>CargoOps</h1>
+
+      <Button variant="primary">
+        Test Button
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
