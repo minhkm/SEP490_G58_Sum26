7a01bf1eea4362eb3c736466ba2c571ae6ad7abf
doquangminh1112003 <minhdqhe176180@fpt.edu.vn>
2026-06-09 13:15:20 +0700

    Voyage Routes
    Voyage List

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,14 @@ require("dotenv").config();
 
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
+const voyageRoutes = require("./routes/voyageRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/voyages", voyageRoutes);
 
 const PORT = process.env.PORT || 5000;
 
