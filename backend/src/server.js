064a5c58b3c04b01831ca648a018c04c633f7f7f
Tongbach <tongbach1210@gmail.com>
2026-07-08 04:10:37 +0700

    feat: report flow

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,6 +24,7 @@ app.use("/api/crews", require("./routes/crewRoutes"));
 app.use("/api/engine-logs", require("./routes/engineLogRoutes"));
 app.use("/api/deck-logs", require("./routes/deckLogRoutes"));
 app.use("/api/notifications", require("./routes/notificationRoutes"));
+app.use("/api/reports", require("./routes/reportRoutes"));
 
 const PORT = process.env.PORT || 5000;
 
