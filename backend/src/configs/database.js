5c60a99348926b1ac12958f02c3de41a3e58cadd
Quy <Quynvhe180901@fpt.edu.vn>
2026-07-11 12:53:34 +0700

    Fix table equipment from ship to voyage

--- backend/src/configs/database.js
+++ backend/src/configs/database.js
@@ -10,6 +10,15 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT,
     dialect: "mysql",
     logging: false, // bật true nếu muốn xem câu SQL Sequelize chạy
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true,
+    },
+    pool: {
+      afterConnect: (conn, done) => {
+        conn.query("SET SESSION sql_mode=''", (err) => done(err, conn));
+      },
+    },
   }
 );
 
