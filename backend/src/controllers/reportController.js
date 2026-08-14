5cc82b716c96ad3a959b6e3231229715217297f4
Tongbach <tongbach1210@gmail.com>
2026-08-15 01:43:34 +0700

    fix : report role

--- backend/src/controllers/reportController.js
+++ backend/src/controllers/reportController.js
@@ -6,6 +6,7 @@ const { buildShiftSnapshot } = require("../services/shiftSnapshotService");
 
 const ROLE_LABELS = {
   Sailor: "Thủy thủ",
+  Crew: "Thủy thủ",
   DeckOfficer: "Sĩ quan boong",
   ChiefOfficer: "Đại phó",
   EngineCrew: "Thợ máy",
