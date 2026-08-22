d9853cee577292183f21256b437be955d0335090
Ankoongggg <annvthe181807@fpt.edu.vn>
2026-08-23 00:35:45 +0700

    Update Leafet Open Map

--- frontend/src/pages/RoutePlannerPage.jsx
+++ frontend/src/pages/RoutePlannerPage.jsx
@@ -744,8 +744,8 @@ export default function RoutePlannerPage() {
               style={{ height: '100%', width: '100%' }}
             >
               <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://carto.com/">CartoDB</a> contributors'
+                url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
               />
               <LocationMarkers
                 waypoints={waypoints}
